bfbad615a56bf12c2a295fe06fd2f79895cc698f
Josh Burton <josh@burton-online.me.uk>
2021-05-28 18:13:12 +0100

    new branch for mcmc

--- src/DelayedKalmanFilter.jl
+++ src/DelayedKalmanFilter.jl
@@ -4,9 +4,11 @@ using ForwardDiff
 using LinearAlgebra
 using Distributions
 using DelimitedFiles
+using Turing
 
 include("kalman_filter_alg.jl")
 include("log_likelihood.jl")
+include("mcmc.jl")
 
 export kalman_filter, calculate_log_likelihood_at_parameter_point, calculate_log_likelihood_and_derivative_at_parameter_point
 
---
+++ test/mcmc_tests.jl
@@ -0,0 +1,10 @@
+using DelayedKalmanFilter
+using DelimitedFiles
+using Test
+
+# @testset "MCMC tests" begin
+#     loading_path = string(pwd(),"/data/");
+#     protein_at_observations = readdlm(string(loading_path,"kalman_filter_test_trace_observations.csv"),',');
+#     model_parameters = [10000.0,5.0,log(2)/30,log(2)/90,1.0,1.0,29.0];
+#     measurement_variance = 10000;
+# end
